b537276ff558ea1a804450208ae6bb2d169a843e
Irham Naufal <inaufal77@gmail.com>
2022-11-15 11:55:04 +0700

    [iOS] setting up the assets

---
+++ MoveOn/MoveOn/Common/Extension/Swift/Color+Default.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Default.swift
+//  MoveOn
+//
+//  Created by Garry on 15/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let primaryPink = Color("primary-pink")
+}
---
+++ MoveOn/MoveOn/Common/Extension/Swift/Image+Default.swift
@@ -0,0 +1,14 @@
+//
+//  Image+Default.swift
+//  MoveOn
+//
+//  Created by Garry on 15/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    static let logoApp = Image("logo-app-icon")
+    static let longLogoApp = Image("logo-app-long")
+}
